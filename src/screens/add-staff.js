2c20aab712efcaca41b893eff16bb0acfadcdc1d
Isaac Newton Tingo <newtone480@gmail.com>
2023-04-03 21:59:40 +0300

    Dee

--- src/screens/add-staff.js
+++ src/screens/add-staff.js
@@ -44,6 +44,7 @@ export default function AddStaff({ route, navigation }) {
   //--------------------
   //--------------------
   //--------------------
+  //--------------------
   async function addStaff() {
     if (!staffName || !staffNumber || !email || !department || !salary) {
       showMyToast({
